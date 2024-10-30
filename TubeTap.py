31a9e8fff709bc43a56733f134ee68f126821a87
Terry-LT <tlatfulla@gmail.com>
2024-10-30 20:03:24 +0100

    Make code readable, updated README, updated releases

--- TubeTap.py
+++ TubeTap.py
@@ -1,65 +1,10 @@
-import instaloader
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
-from RedDownloader import RedDownloader
-
-def remove_white_space(text):
-    return "".join(text.split())
-def youtube_download(mp3_or_mp4,url_input):
-    try:
-        yt = YouTube(url_input, on_progress_callback=on_progress)
-        print(yt.title)
-        if remove_white_space(mp3_or_mp4) == "+":
-            ys = yt.streams.get_highest_resolution()
-            ys.download()
-            print("The video mp4 was downloaded!")
-        else:
-            ys = yt.streams.get_audio_only()
-            ys.download(mp3=True)
-            print("The audio mp3 was downloaded!")
-    except Exception as e:
-        print(f"Error message: {e}")
-
-def instagram_download(post_url):
-    # Create an Instaloader instance
-    loader = instaloader.Instaloader()
-    # Extract the shortcode from the URL (if it's a full URL)
-    shortcode = post_url.split("/")[-2]
-    # Download the post
-    try:
-        post = instaloader.Post.from_shortcode(loader.context, shortcode)
-        loader.download_post(post, target=f"{post.owner_username}_{post.shortcode}")
-        print("Download successful!")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-def tik_tok_download(post_url):
-    pass
-
-def reddit_download(post_url):
-    try:
-        RedDownloader.Download(post_url)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+from download import Video
 
 def main():
-    commands = {
-        "y":"Youtube",
-        "i": "Instagram",
-        "r":"Reddit"
-    }
-    print(commands)
-    social_media = input("Write the name of a social media. If you want to see the available list of themes, type help:")
-    social_media = remove_white_space(social_media)
-    url_input = input("Url:")
-    if social_media == "y":
-        mp3_or_mp4 = input("Select format (Type + for MP4 or - for MP3):")
-        youtube_download(mp3_or_mp4, url_input)
-    elif social_media == "i":
-        instagram_download(url_input)
-    elif social_media == "r":
-        reddit_download(url_input)
-
+    video_url = input("Url:")
+    video = Video(video_url)
+    video.download()
     input("Press to quit...")
+
 if __name__ == "__main__":
     main()
---
+++ download.py
@@ -0,0 +1,75 @@
+import validators
+import instaloader
+from urllib.parse import urlparse
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+from RedDownloader import RedDownloader
+
+def remove_white_space(text):
+    return "".join(text.split())
+
+class Video:
+    video_url = ""
+    supported_social_media_links = [
+        "www.youtube.com",
+        "www.youtu.be",
+        "www.instagram.com",
+        "www.reddit.com"
+    ]
+    domain = None
+    def __init__(self,video_url):
+        if validators.url(video_url):
+            domain = urlparse(video_url).netloc
+            if domain not in self.supported_social_media_links:
+                print(f"Currently, we do not support domain {domain}")
+            else:
+                self.domain = domain
+                self.video_url = video_url
+        else:
+            print("The url is not valid")
+    def _youtube_download(self,mp3_or_mp4):
+        try:
+            yt = YouTube(self.video_url, on_progress_callback=on_progress)
+            print(yt.title)
+            if remove_white_space(mp3_or_mp4) == "+":
+                ys = yt.streams.get_highest_resolution()
+                ys.download()
+                print("The video mp4 was downloaded!")
+            else:
+                ys = yt.streams.get_audio_only()
+                ys.download(mp3=True)
+                print("The audio mp3 was downloaded!")
+        except Exception as e:
+            print(f"Error message: {e}")
+
+    def _instagram_download(self):
+        # Create an Instaloader instance
+        loader = instaloader.Instaloader()
+        # Extract the shortcode from the URL (if it's a full URL)
+        shortcode = self.video_url.split("/")[-2]
+        # Download the post
+        try:
+            post = instaloader.Post.from_shortcode(loader.context, shortcode)
+            loader.download_post(post, target=f"{post.owner_username}_{post.shortcode}")
+            print("Download successful!")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def _tik_tok_download(self):
+        pass
+
+    def _reddit_download(self):
+        try:
+            RedDownloader.Download(self.video_url)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    def download(self):
+        if self.domain is not None:
+            if (self.domain == self.supported_social_media_links[0]) or (
+                    self.domain == self.supported_social_media_links[1]):
+                mp3_or_mp4 = input("Select format (Type + for MP4 or - for MP3):")
+                self._youtube_download(mp3_or_mp4)
+            elif self.domain == self.supported_social_media_links[2]:
+                self._instagram_download()
+            elif self.domain == self.supported_social_media_links[3]:
+                self._reddit_download()
